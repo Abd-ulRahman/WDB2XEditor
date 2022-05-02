b59b11be4cab0ed2aed84fbd5663cf8bb9ccadac
AbdulRahman Alkhamees <zbestofzbest@yahoo.com>
2022-05-03 00:51:45 +0300

    Update WDC3.cs

--- WDBXEditor/Reader/FileTypes/WDC3.cs
+++ WDBXEditor/Reader/FileTypes/WDC3.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WDBXEditor.Storage;
 using static WDBXEditor.Common.Constants;
+using WDBXEditor.Commons;
 
 namespace WDBXEditor.Reader.FileTypes
 {
