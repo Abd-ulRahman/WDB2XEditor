01b3f55967c3645297b861651ab2c2c9b7f16a30
AbdulRahman Alkhamees <zbestofzbest@yahoo.com>
2022-05-03 00:48:03 +0300

    Add files via upload

---
+++ WDBXEditor/Reader/DBStructs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CS0649
+#pragma warning disable IDE0044
+
+namespace WDBXEditor.Commons
+{
+    public enum CompressionType
+    {
+        None = 0,
+        Immediate = 1,
+        Sparse = 2,
+        Pallet = 3,
+        PalletArray = 4,
+        SignedImmediate = 5
+    }
+}
