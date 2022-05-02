602663758322b2a050a85ce0965aafa097719a56
AbdulRahman Alkhamees <zbestofzbest@yahoo.com>
2022-05-03 00:49:12 +0300

    Update ColumnStructureEntry.cs

--- WDBXEditor/Reader/ColumnStructureEntry.cs
+++ WDBXEditor/Reader/ColumnStructureEntry.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static WDBXEditor.Common.Constants;
+using WDBXEditor.Commons;
 
 namespace WDBXEditor.Reader
 {
@@ -12,7 +13,29 @@ namespace WDBXEditor.Reader
 		public ushort RecordOffset { get; set; }
 		public ushort Size { get; set; }
 		public uint AdditionalDataSize { get; set; }
-		public CompressionType CompressionType { get; set; }
+
+		private CompressionType compressionType;
+		public CompressionType CompressionType
+		{
+			get { return compressionType; }
+			set
+			{
+				if (value >= CompressionType.None && value <= CompressionType.SignedImmediate)
+				{
+					compressionType = value;
+				}
+				else
+					try
+					{
+						if (value <= CompressionType.SignedImmediate && value >= CompressionType.None)
+						{
+							compressionType = value;
+						}
+					}
+					catch (Exception) { }
+			}
+		}
+
 		public int BitOffset { get; set; }  // used as common data column for Sparse
 		public int BitWidth { get; set; }
 		public int Cardinality { get; set; } // flags for Immediate, &1: Signed
