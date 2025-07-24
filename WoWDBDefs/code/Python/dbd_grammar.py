960b7848acc1202adb527cc7db04207fe5a4589e
Abdulrahman Alkhamees <zbestofzbest@yahoo.com>
2025-07-24 16:44:46 +0300

    Delete WoWDBDefs directory

--- WoWDBDefs/code/Python/dbd_grammar.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-
-import dbd
-import modgrammar
-import sys
-
-sys.stdout.writelines(modgrammar.generate_ebnf(dbd.dbd_file, wrap=None))
--- WoWDBDefs/code/Python/dump_relations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./dump_relations.py \
-  --definitions ../../definitions/  \
-  $(ls ../../definitions/*Visual* ../../definitions/*Model* \
-      | sed -e 's,../../definitions/,--only ,' -e 's,.dbd$,,') \
-  | dot -Tpdf > t.pdf
